6fa56b50c562a044f90ddd4ce6c53e7ea338cc35
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-09 11:37:08 +0000

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-todo2.0/",
   plugins: [react()],
-})
+});
